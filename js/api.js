8552e7212d0f41c2d3c31ef6b8cdf39e62228e82
Jeferson Brendo <jefersonbrendo077@gmail.com>
2025-06-12 23:11:43 -0300

    realizar o tratamento de erros

--- js/api.js
+++ js/api.js
@@ -2,9 +2,10 @@ const api = {
     async buscarPensamentos() {
         try {
             const response = await fetch('http://localhost:3000/pensamentos');
-            return response.json();
+            return await response.json();
         } catch (err) {
             alert('Erro ao buscar pensamentos: ');
+            throw err;
         }
     }
 }
