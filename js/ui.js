c69dbd676bd3fce34b14066640325f8d45b6f818
Jeferson Brendo <jefersonbrendo077@gmail.com>
2025-06-14 21:14:01 -0300

    adiciona a estrutura de cada item na lista de pensamentos

--- js/ui.js
+++ js/ui.js
@@ -1,23 +1,39 @@
 import api from "./api.js";
+const listaPensamentos = document.querySelector("#lista-pensamentos");
 
 const ui = {
   async renderizarPensamentos() {
-    const listaPensamentos = document.querySelector("#lista-pensamentos");
-
     try {
       const pensamentos = await api.buscarPensamentos();
-      pensamentos.forEach((pensamento) => {
-        listaPensamentos.innerHTML += `
-            <li class="li-pensamento" data-id="${pensamento.id}">
-            <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-            <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-            <div class="pensamento-autoria">${pensamento.autoria}</div>
-            </li>
-        `;
-      });
+      pensamentos.forEach(ui.adicionarPensamentoNaLista())
     } catch (err) {
       alert("Erro ao renderizar pensamentos: " + err.message);
     }
+  },
+
+  adicionarPensamentoNaLista(pensamento) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", pensamento.id);
+    li.classList.add("li-pensamento");
+
+    const iconeAspas = document.createElement("img");
+    iconeAspas.src = "assets/imagens/aspas-azuis.png";
+    iconeAspas.alt = "Aspas azuis";
+    iconeAspas.classList.add("icone-aspas");
+
+    const pensamentoConteudo = document.createElement("div");
+    pensamentoConteudo.textContent = pensamento.conteudo;
+    pensamentoConteudo.classList.add("pensamento.conteudo");
+    
+    const pensamentoAutoria = document.createElement("div");
+    pensamentoAutoria.textContent = pensamento.autoria;
+    pensamentoAutoria.classList.add("pensamento.autoria");
+    
+    li.appendChild(iconeAspas);
+    li.appendChild(pensamentoConteudo);
+    li.appendChild(pensamentoAutoria);
+    listaPensamentos.appendChild(li)
+    
   },
 };
 
