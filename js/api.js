ecd70d6b8cc1cb796986fe6593a1df289e099415
Jeferson Brendo <jefersonbrendo077@gmail.com>
2025-06-15 23:59:54 -0300

    update api.js

--- js/api.js
+++ js/api.js
@@ -37,7 +37,7 @@ const api = {
 
   async editarPensamento(pensamento) {
     try {
-      const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -46,7 +46,7 @@ const api = {
       });
       return await response.json();
     } catch (err) {
-      alert("Erro ao editar pensamentos");
+      alert("Erro ao editar pensamento");
       throw err;
     }
   },
