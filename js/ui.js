d5067cd605b259137ad16a538487eb42751fee97
Jeferson Brendo <jefersonbrendo077@gmail.com>
2025-06-15 23:40:58 -0300

    adiciona a função de preenchimento do formulario automatico ao clicar em editar pensamento

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,13 @@ import api from "./api.js";
 const listaPensamentos = document.querySelector("#lista-pensamentos");
 
 const ui = {
+  async preencherFormulario(pensamentoId) {
+    const pensamento = await api.buscarPensamentoPorId(pensamentoId);
+    document.getElementById("pensamento-id").value = pensamento.id;
+    document.getElementById("pensamento-conteudo").value = pensamento.conteudo;
+    document.getElementById("pensamento-autoria").value = pensamento.autoria;
+  },
+
   limparFormulario() {
     document.getElementById("pensamento-form").reset();
   },
@@ -9,7 +16,7 @@ const ui = {
   async renderizarPensamentos() {
     try {
       const pensamentos = await api.buscarPensamentos();
-      pensamentos.forEach(ui.adicionarPensamentoNaLista)
+      pensamentos.forEach(ui.adicionarPensamentoNaLista);
     } catch (err) {
       alert("Erro ao renderizar pensamentos: " + err.message);
     }
@@ -28,16 +35,29 @@ const ui = {
     const pensamentoConteudo = document.createElement("div");
     pensamentoConteudo.textContent = pensamento.conteudo;
     pensamentoConteudo.classList.add("pensamento-conteudo");
-    
+
     const pensamentoAutoria = document.createElement("div");
     pensamentoAutoria.textContent = pensamento.autoria;
     pensamentoAutoria.classList.add("pensamento-autoria");
-    
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("botao-editar");
+    botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id);
+
+    const iconeEditar = document.createElement("Img");
+    iconeEditar.src = "assets/imagens/icone-editar.png";
+    iconeEditar.alt = "Editar";
+    botaoEditar.appendChild(iconeEditar);
+
+    const icones = document.createElement("div")
+    icones.classList.add("icones")
+    icones.appendChild(botaoEditar)
+
     li.appendChild(iconeAspas);
     li.appendChild(pensamentoConteudo);
     li.appendChild(pensamentoAutoria);
-    listaPensamentos.appendChild(li)
-    
+    li.appendChild(icones)
+    listaPensamentos.appendChild(li);
   },
 };
 
